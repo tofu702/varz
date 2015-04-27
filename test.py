618b2af508a210533341d33357c53a48acd5f26a
Steven Stanek <steven@shopkick.com>
2015-04-26 21:27:52 -0700

    Slightly better tests

--- test.py
+++ test.py
@@ -4,10 +4,17 @@ import client
 def main():
   c = client.VARZClient()
   c.setup()
+  print "*****Running increments & adds (no output)*****"
   c.counter_increment("test_counter")
   c.sampler_add("test_sampler", 2)
   c.counter_increment("test_counter2")
+  print "*****Running all_list*****"
   print c.all_list()
+  print "*****Running all_dump*****"
+  print c.all_dump()
+  print "*****Running all_flush*****"
+  print c.all_flush();
+  print "*****Running all_dump*****"
   print c.all_dump()
 
 if __name__ == "__main__":
