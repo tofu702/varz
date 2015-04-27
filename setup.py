9d3b774306dc63b81e5b8a683a5481df0a91ab17
Steven Stanek <steven@shopkick.com>
2015-04-26 22:01:41 -0700

    Increment setup.py version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="varz_client",
-      version='0.1a', 
+      version='0.2a', 
       package_dir={"varz_client": ""},
       packages=["varz_client"],
      )
