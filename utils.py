80a4c503675026c1274e2f1c20de6e3363cfb0f6
Steven Stanek <steven@shopkick.com>
2015-05-17 20:35:12 -0700

    Make datetime_to_sec_since_epoch return an int and fix epoch_sec_to_minutes_since_epoch

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import time
 def datetime_to_sec_since_epoch(dt):
   '''Take a python datetime object and convert it to seconds since the unix epoch. Not timezone
      aware'''
-  return time.mktime(dt.timetuple())
+  return int(time.mktime(dt.timetuple()))
 
 def sec_since_epoch_to_datetime(sec_since_epoch):
   '''Take some number seconds since the epoch and convert them to a python datetime object. Not
@@ -13,4 +13,4 @@ def sec_since_epoch_to_datetime(sec_since_epoch):
 
 def epoch_sec_to_minutes_since_epoch(sec_since_epoch):
   '''Return the number of minutes since the epoch (quite a large number)'''
-  return (sec_since_epoch / 60) % 60
+  return (sec_since_epoch / 60)
